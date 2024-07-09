3cc8f30f50ead3a9de505408579cd0f4c0ec5c28
meme <aotameru@gmail.com>
2024-07-09 09:48:41 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -18,32 +18,32 @@ document.addEventListener('DOMContentLoaded', () => {
             answer: "ふれいしあ",
             src:"https://docs.google.com/forms/d/e/1FAIpQLSf2FVI6X03VW8DfxAAYUGgmcLyagyNWLFgeLamREgTG6EKngg/viewform?embedded=true",
             hint:hint0,
-            img:"./assets/nazo1"
+            img:"./assets/nazo1.jpg"
         },{ id: 1, 
             answer: "かろる",
             src:"https://docs.google.com/forms/d/e/1FAIpQLSetdY0-WXWiU935T7sa9570tHNfIKyZ98973v-R2pjxpDDduQ/viewform?embedded=true",
             hint:hint1,
-            img:"./assets/nazo2"
+            img:"./assets/nazo2.jpg"
         },{ id: 2, 
             answer: "ろざりー",
             src:"https://docs.google.com/forms/d/e/1FAIpQLSciB9li1luoz6ywYT1LySv9wmjY4vMp5L_VpcoiLJjH9WOksQ/viewform?embedded=true",
             hint:hint2,
-            img:"./assets/nazo3"
+            img:"./assets/nazo3.jpg"
         },{ id: 3, 
             answer: "ぷりしら",
             src:"https://docs.google.com/forms/d/e/1FAIpQLSdJ9HRjR06zX0203V6T59sv1sgKGIILlZcfraURPs6BZOMFew/viewform?embedded=true",
             hint:hint3,
-            img:"./assets/nazo4"
+            img:"./assets/nazo4.jpg"
         },{ id: 4, 
             answer: "べる",
             src:"https://docs.google.com/forms/d/e/1FAIpQLSdziy5_jqLEqeC9jrxsKZBS5yJuNgjGXM4Bdy-W0HpWKmQigA/viewform?embedded=true",
             hint:hint4,
-            img:"./assets/nazo5"
+            img:"./assets/nazo5.jpg"
         },{ id: 5, 
             answer: "あいびー",
             src:"https://docs.google.com/forms/d/e/1FAIpQLScsPmYyllk0dZfRaTYK4-aIjUlR82Uic-L2EWpTBZekcr_uWg/viewform?embedded=true",
             hint:hint5,
-            img:"./assets/nazo6"
+            img:"./assets/nazo6.jpg"
         },
     ];
 
@@ -58,14 +58,31 @@ document.addEventListener('DOMContentLoaded', () => {
                     hintBox.innerHTML = ""
                     hintText.innerHTML = ""
 
+                    const img = document.getElementById('quizImg');
+                    img.src = quizList[index]['img'];
+
     for(i = 1;i <= quizList[index].hint.length;++i){
         const hintBox = document.getElementById('hintBox');
         const hintBtn = document.createElement('button')
         hintBtn.innerHTML = "ヒント" + i;
         hintBox.appendChild(hintBtn);
     }
-        const gForm = document.getElementById('gForm');
-        gForm.src = quizList[index]['src'];
+        const answerContainer = document.getElementById('answerContainer');
+        const answerContainer2 = document.getElementById('answerContainer2');
+answerContainer.innerHTML = "";
+answerContainer2.innerHTML = "";
+
+answerContainer.href = quizList[index]['src'];
+
+var answerButton1 = document.createElement("button");
+answerButton1.textContent = "回答する(googleForm)";
+answerContainer.appendChild(answerButton1);
+
+var answerButton2 = document.createElement("button");
+answerButton2.textContent = "答えを見る(準備中)";
+answerContainer2.appendChild(answerButton2);
+
+
 
         const hintButtons = hintBox.querySelectorAll('button')
         hintButtons.forEach((buttonHint,no) => {
@@ -77,7 +94,10 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-
+        
 
         })});
+        answerContainer2.addEventListener('click',  () => {
+            alert("準備中です");
+        })
     });
